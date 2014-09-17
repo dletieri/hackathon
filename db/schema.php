11a93a834017a81ae787b56495b408efc7dd112e
Christian Wong <dev@christianwong.com>
2014-09-16 21:16:10 -0300

    added schema db

---
+++ db/schema.php
@@ -0,0 +1,76 @@
+<?php
+	include 'util/db_utils.php';
+
+	$hostname = "localhost";	// Vamos considerar localhost ou máquina local
+	$username = "userTeste"; 	// Username é userTeste
+	$password = "passTeste"; 	// Password é passTeste (Cuidado com maiúsculas e minúsculas
+	$database = "BigData";
+
+	// Connect to Database
+	$con=connect($hostname, $username, $password);
+	executeQuery($con, "DROP DATABASE $database");
+	executeQuery($con, "CREATE DATABASE $database");
+
+	// Create tables
+	executeQuery($con, "CREATE TABLE $database.tb_entity (
+		`objid` bigint(20) unsigned NOT NULL,
+		`name` text NOT NULL,
+		`type` text NOT NULL,
+
+		PRIMARY KEY (objid)
+		) AUTO_INCREMENT=1");
+
+	executeQuery($con, "CREATE TABLE $database.tb_location (
+		`objid` bigint(20) unsigned NOT NULL,
+		`latitude` varchar(64) NOT NULL,
+		`longitude` varchar(64) NOT NULL,
+		`address` text NOT NULL,
+		`city` text NOT NULL,
+		`state` text NOT NULL,
+		`country` text NOT NULL,
+		`zip` text NOT NULL,
+
+		PRIMARY KEY (objid)
+		) AUTO_INCREMENT=1 ;");
+
+	executeQuery($con, "CREATE TABLE $database.tbnn_entity_location (
+		`entity_id` bigint(20) unsigned NOT NULL,
+		`location_id` bigint(20) unsigned NOT NULL,
+
+		INDEX(entity_id),
+		FOREIGN KEY (entity_id)
+			REFERENCES tb_entity(objid),
+
+		INDEX(location_id),
+		FOREIGN KEY (location_id)
+			REFERENCES tb_location(objid)
+		)");
+
+	executeQuery($con, "CREATE TABLE $database.tbnn_entity_media (
+		`entity_id` bigint(20) unsigned NOT NULL,
+		`media_id` bigint(20) unsigned NOT NULL
+		)");
+
+	executeQuery($con, "CREATE TABLE $database.tb_metadata (
+		`objid` bigint(20) unsigned NOT NULL,
+		`meta_type` text NOT NULL,
+		`meta_info` text NOT NULL,
+		`entity_id` bigint(20) unsigned NOT NULL,
+
+		PRIMARY KEY (objid),
+		INDEX(entity_id),
+		FOREIGN KEY (entity_id)
+			REFERENCES tb_entity(objid)
+		) AUTO_INCREMENT=1 ;");
+
+	executeQuery($con, "CREATE TABLE $database.tb_media (
+		`objid` bigint(20) unsigned NOT NULL,
+		`image` text,
+		`video` text,
+		`type` text NOT NULL,
+		PRIMARY KEY (objid)
+		) AUTO_INCREMENT=1 ;");
+	
+	disconnect($con);
+	echo "<br>Success!<br/>";
+?>
---
+++ db/util/db_utils.php
@@ -0,0 +1,36 @@
+<?php
+
+function connect($hostname, $username, $password) {
+	$con=mysqli_connect($hostname, $username, $password);
+	if (mysqli_connect_errno()) {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  return NULL;
+	}
+
+	return $con;
+}
+
+function selectDB($con, $database) {
+	if ($db=mysql_select_db($database/*, $con*/)) {
+	  echo "Failed to select DB: '$database' " . mysqli_connect_error();
+	  return FALSE;
+	}
+	//echo "selected db: $db<br>";
+
+	return TRUE;
+}
+
+function executeQuery($con,$sql) {
+	if (mysqli_query($con,$sql)) {
+	  return TRUE;
+	}
+
+	echo "Error executing query: $sql". $sql . mysqli_error($con);
+	return FALSE;
+}
+
+function disconnect($con) {
+	mysqli_close($con);
+}
+
+?>
